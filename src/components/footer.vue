dda3e56415e84335f47f9e5cf3e54a2867c2583d
Valeria Silva <valeriac.dasilva26@gmail.com>
2024-09-17 15:26:39 -0300

    add menu and icons

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,23 +4,65 @@
       <h1>Tenho interesse</h1>
       <div class="subscription-form">
         <input type="email" placeholder="Email" />
-        <button>Tenho interesse</button>
+        <Button name="Tenho interesse" type="outline" />
+      </div>
+    </div>
+    <div class="menu-container">
+      <Menu />
+      <div class="social-container">
+        <img src="../assets/images/icons/linkedin.png" alt="" />
+        <img src="../assets/images/icons/instagran.png" alt="" />
       </div>
     </div>
     <p>Copyright © 2024 buildweb</p>
   </footer>
 </template>
 
-<style scoped>
+<script setup>
+import Button from './button.vue';
+import Menu from './menu.vue';
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variables.scss';
+
 p {
   text-align: center;
-  font-size: 14px;
 }
+
 .subscription-container {
   display: flex;
   gap: 29px;
   text-align: center;
   align-items: center;
   justify-content: center;
+
+  .subscription-form {
+    background: $ghost-white;
+
+    input {
+      width: 200px;
+      height: 30px;
+      border: none;
+      outline-color: 0;
+      padding-left: 10px;
+    }
+  }
+}
+.menu-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  z-index: 1;
+  position: relative;
+  margin: 100px 100px 70px 0;
+  gap: 34px;
+  .social-container {
+    display: flex;
+    gap: 20px;
+    img {
+      width: 28px;
+    }
+  }
 }
 </style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,46 +1,67 @@
 <template>
-    <div class="menu">
-        <router-link v-for="item in props.item" :key="item.label" :to="item.route"
-            :class="['menu-item', { active: isActiveRoute(item.route) }]">
-            <div>
-                <span>{{ item.label }}</span>
-            </div>
-        </router-link>
-    </div>
+  <div class="menu">
+    <router-link
+      v-for="item in menuItem"
+      :key="item.label"
+      :to="item.route"
+      :class="['menu-item', { active: isActiveRoute(item.route) }]"
+    >
+      <div>
+        <span>{{ item.label }}</span>
+      </div>
+    </router-link>
+  </div>
 </template>
 
 <script setup>
-import { ref, defineProps } from "vue";
-import { useRoute } from "vue-router";
+import { ref, defineProps } from 'vue';
+import { useRoute } from 'vue-router';
 
+const menuItem = [
+  {
+    label: 'Home',
+    route: '/',
+  },
+  {
+    label: 'Sobre nós',
+    route: '/about',
+  },
+  {
+    label: 'Serviços',
+    route: '/service',
+  },
+  {
+    label: 'Contato',
+    route: '/contact',
+  },
+];
 const props = defineProps({
-    item: {
-        type: Array,
-        required: true,
-    },
+  item: {
+    type: Array,
+    required: false,
+  },
 });
 
 const route = useRoute();
 
 const isActiveRoute = (routePath) => {
-    return route.path === routePath;
+  return route.path === routePath;
 };
-
 </script>
 
 <style lang="scss" scoped>
-@import "../assets/scss/variables.scss";
+@import '../assets/scss/variables.scss';
 
 .menu {
-    display: flex;
-    gap: 20px;
+  display: flex;
+  gap: 20px;
 
-    .menu-item {
-        color: $ghost-white;
-    }
+  .menu-item {
+    color: $ghost-white;
+  }
 
-    .active {
-        color: $yellow-web;
-    }
+  .active {
+    color: $yellow-web;
+  }
 }
 </style>
