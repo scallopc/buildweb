b2bbc8667bd724788ee6bd94578c7f31792c7ca8
Valeria Silva <valeriac.dasilva26@gmail.com>
2024-09-17 10:36:23 -0300

    footer updates

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,10 +1,26 @@
 <template>
-  <footer><p>Copyright © 2024 buildweb</p></footer>
+  <footer>
+    <div class="subscription-container">
+      <h1>Tenho interesse</h1>
+      <div class="subscription-form">
+        <input type="email" placeholder="Email" />
+        <button>Tenho interesse</button>
+      </div>
+    </div>
+    <p>Copyright © 2024 buildweb</p>
+  </footer>
 </template>
 
 <style scoped>
-footer {
+p {
   text-align: center;
   font-size: 14px;
+}
+.subscription-container {
+  display: flex;
+  gap: 29px;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
 }
 </style>
